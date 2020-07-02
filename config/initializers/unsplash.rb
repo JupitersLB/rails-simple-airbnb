c18f0e7f112b518e2cbee4268b38af53ff3450b2
Liam Baker <liam.baker2310@gmail.com>
2020-07-02 22:26:38 +0900

    models and controllers made

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,6 @@
+Unsplash.configure do |config|
+  config.application_access_key = 'A1TML45OE0bvoDf1P3bK0Xl_BK0Trbux4GmmcsuJhmM'
+  config.application_secret = 'c2YwzN_MWQLGNT6vGpFZP-v5Ebf4lK9wHqD7bAjH-QY'
+  config.application_redirect_uri = 'http://localhost:3000/oauth/callback'
+  config.utm_source = 'simple-airbnb'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Deleting all flats'
+
+Flat.destroy_all
+
+puts 'Creating database'
+
+20.times do
+  Flat.create!(
+    name: Faker::Movies::HarryPotter.location,
+    address: Faker::Address.full_address,
+    description: Faker::Marketing.buzzwords,
+    price_per_night: Faker::Number.number(digits: 3),
+    number_of_guests: Faker::Number.number(digits: 1),
+    picture_url: Faker::LoremFlickr.image(search_terms: ['house'])
+  )
+end
+
+puts 'Finished making flats'
